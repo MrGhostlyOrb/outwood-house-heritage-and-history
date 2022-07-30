60a682c7d48f89e37d1d9ffd821354d66bb5a01b
Ben Cooper <ben.f.cooper@gmail.com>
2022-07-30 17:59:38 +0100

    feat(SERVER): Use http/2

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-// Serve a static html file
-const compression = require('compression');
+// Serve an express app over http/2
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const spdy = require('spdy');
+const fs = require('fs');
 
-// enable text compression
-app.use(compression());
-
+// set public folder as root
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+// Serve the app at the root url
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-// Bind to heroku port or default to 3000
-const port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Server running on port ' + port);
+// Serve over http/2 using spdy
+const options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt')
+}
+
+
+spdy.createServer(options, app).listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server running at port ${port}`);
+    }
+});
